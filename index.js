4ab03427e0f671b04e56cbd9959b3abdf6fa19c6
Olivier Graber <olivier.graber@heig-vd.ch>
2024-04-16 11:12:05 +0200

    create user api

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDFzOgqAIICMGFvanomxjBXHcE3xWNkL94",
@@ -53,6 +54,27 @@ io.on("connection", (socket) => {
 
 app.get("/", (req, res) => {
   res.send("Server is up and running");
+});
+
+// Middleware pour analyser les corps de requête
+app.use(bodyParser.json());
+
+// Point de terminaison API pour créer un utilisateur
+app.post("/create-user", async (req, res) => {
+  try {
+    const userData = req.body;
+    const userId = Date.now().toString(); // Utilisez l'ID utilisateur généré par Firebase Auth
+    await set(ref(database, `users/${userId}`), userData);
+    res
+      .status(200)
+      .send({ message: "Utilisateur créé avec succès", userId: userId });
+  } catch (error) {
+    console.error("Erreur de création d'utilisateur", error);
+    res.status(500).send({
+      message: "Erreur lors de la création de l'utilisateur",
+      error: error.message,
+    });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
